aae428ff0894c414f80dc1cc208a8d1a488e4a1d
bismanroopsingh <135754093+bismanroopsingh@users.noreply.github.com>
2026-06-13 03:08:48 +0530

    Replace paragraph chunking with semantic chunking

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from pdf_processing import extract_text_from_pdf
-from chunking import paragraph_chunk_text
+from chunking import semantic_chunk_text
 
 st.set_page_config(page_title="EduCast AI")
 
-st.title("🎧 EduCast AI")
+st.title("EduCast AI")
 st.subheader("PDF Text Extraction")
 
 uploaded_file = st.file_uploader(
@@ -18,7 +18,7 @@ if uploaded_file is not None:
 
     text = extract_text_from_pdf(uploaded_file)
 
-    chunks = paragraph_chunk_text(text)
+    chunks =semantic_chunk_text(text)
     st.success(f"Created {len(chunks)} chunks")
     st.subheader("Extracted Text")
     for i, chunk in enumerate(chunks):
