e4aa1c5096cd52ed05c6f7ee039bed348601164e
bismanroopsingh <135754093+bismanroopsingh@users.noreply.github.com>
2026-06-13 03:07:44 +0530

    Add embedding generation using SentenceTransformer

---
+++ embedding.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+def generate_embeddings(texts):
+    """
+    Generate embeddings for a list of texts.
+
+    Parameters:
+        texts (list): List of sentences/chunks
+
+    Returns:
+        embeddings (numpy array)
+    """
+
+    embeddings = model.encode(texts)
+
+    return embeddings
